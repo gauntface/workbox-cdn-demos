acc32896403d464812e5a02706415c6bbeca72e5
Matt Gaunt <matt@gauntface.co.uk>
2017-10-11 09:48:52 -0400

    Adding more entries

---
+++ public/static/demo/workbox-precaching/sw-1.js
@@ -0,0 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/v3.0.0-alpha/workbox-sw.dev.js');
+
+const wb = new WorkboxSW({
+  debug: true
+});
+
+wb.precaching.precache([
+  {url: '/demo/workbox-precaching', revision: '1'},
+  'test-file.txt',
+  'version-1-file.txt'
+]);
---
+++ public/static/demo/workbox-precaching/sw-2.js
@@ -0,0 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/v3.0.0-alpha/workbox-sw.dev.js');
+
+const wb = new WorkboxSW({
+  debug: true
+});
+
+wb.precaching.precache([
+  {url: '/demo/workbox-precaching', revision: '2'},
+  'test-file.txt',
+  'version-2-file.txt'
+]);
