2d00648e595a8547c927bd14ded240d029b52725
Matt Gaunt <matt@gauntface.co.uk>
2017-10-11 10:29:37 -0400

    Adding some small logic for demos, guide and docs

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,57 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const fs = require('fs-extra');
+const path = require('path');
 
-const modules = [
-  {name: 'workbox-core'},
-  {name: 'workbox-precaching'},
-  {name: 'workbox-sw'},
-  {name: 'workbox-routing'},
+const workboxModules = [
+  {
+    name: 'workbox-core',
+  },
+  {
+    name: 'workbox-precaching',
+    guide: 'https://docs.google.com/a/google.com/document/d/1lM_fVQZpohD8N1QLuxobmrYrq9lOJh4jYF3MJhNrMvs/edit?usp=sharing',
+  },
+  {
+    name: 'workbox-routing',
+  },
+  {
+    name: 'workbox-runtime-caching',
+  },
+  {
+    name: 'workbox-background-sync',
+  },
+  {
+    name: 'workbox-sw',
+  },
 ];
+
+// Has Docs
+workboxModules.map((workboxModule) => {
+  const docPath = path.join(
+    __dirname, '..', 'public', 'reference-docs',
+    `module-${workboxModule.name}.html`
+  );
+  let exists = false;
+  try {
+    fs.accessSync(docPath);
+    exists = true;
+  } catch(err) {}
+  workboxModule.hasDocs = exists;
+});
+
+// Has Docs
+workboxModules.map((workboxModule) => {
+  const docPath = path.join(
+    __dirname, 'views', 'demo', `${workboxModule.name}.hbs`
+  );
+  let exists = false;
+  try {
+    fs.accessSync(docPath);
+    exists = true;
+  } catch(err) {}
+  workboxModule.hasDemo = exists;
+});
 
 const app = express();
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
@@ -16,14 +60,14 @@ app.set('view engine', 'hbs');
 app.get('/', function (req, res) {
   res.render('home', {
     title: 'Workbox V3',
-    modules,
+    modules: workboxModules,
   });
 });
 
 app.get('/demo/:moduleName', function (req, res) {
   res.render(`demo/${req.params.moduleName}`, {
     title: `${req.params.moduleName} Demo`,
-    modules,
+    modules: workboxModules,
   });
 });
 
