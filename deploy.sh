6b4068b9541b38e5e3980387b350ca2a00ab25dd
Matt Gaunt <matt@gauntface.co.uk>
2017-10-11 15:50:14 -0400

    Adding logging for travis

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,77 @@
+echo "Cloning workbox repo"
+echo ""
+echo ""
 git clone -b v3 https://github.com/GoogleChrome/workbox.git --depth=1 ./workbox/
+
+echo ""
+echo ""
+echo "cd in workbox"
+echo ""
+echo ""
 cd ./workbox/
+
+echo ""
+echo ""
+echo "npm install"
+echo ""
+echo ""
 npm install
+
+echo ""
+echo ""
+echo "Build Docs"
+echo ""
+echo ""
 gulp docs:build
-rm -rf ../public/reference-docs/
-mv ./docs/ ../public/reference-docs/
+
+echo ""
+echo ""
+echo "cd out of workbox"
+echo ""
+echo ""
 cd ..
+
+echo ""
+echo ""
+echo "Remove public/reference-docs/"
+echo ""
+echo ""
+rm -rf ./public/reference-docs/
+
+echo ""
+echo ""
+echo "pwd && ls -l"
+echo ""
+echo ""
+pwd
+ls -l
+
+echo ""
+echo ""
+echo "mv ./workbox/docs/ ./public/reference-docs/"
+echo ""
+echo ""
+mv ./workbox/docs/ ./public/reference-docs/
+
+echo ""
+echo ""
+echo "rm -rf ./workbox/"
+echo ""
+echo ""
 rm -rf ./workbox/
+
+echo ""
+echo ""
+echo "Update modules.json"
+echo ""
+echo ""
 node ./update-modules.js
+
+echo ""
+echo ""
+echo "Deploy to Firebase"
+echo ""
+echo ""
 if [ -z "$FIREBASE_TOKEN" ]; then
   firebase deploy
 else
