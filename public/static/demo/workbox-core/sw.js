4e38ea74097f13ef878b9a0100634828bece142d
Matt Gaunt <matt@gauntface.co.uk>
2017-10-13 11:42:52 -0400

    Altering the version

--- public/static/demo/workbox-core/sw.js
+++ public/static/demo/workbox-core/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/v3.0.0-alpha/workbox-sw.dev.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.17/workbox-sw.dev.js');
 
 const wb = new WorkboxSW({
   debug: true
