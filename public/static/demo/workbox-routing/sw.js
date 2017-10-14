785e73b4904e4532168565afafde9e116437fc43
Matt Gaunt <matt@gauntface.co.uk>
2017-10-14 15:24:54 -0400

    Started work on the route but had a few issues

--- public/static/demo/workbox-routing/sw.js
+++ public/static/demo/workbox-routing/sw.js
@@ -1,10 +1,22 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.17/workbox-sw.dev.js');
+// Would be good to swap with workbox-sw
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.17/workbox-core.dev.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.17/workbox-routing.dev.js');
 
+/*
 const wb = new WorkboxSW({
   debug: true
 });
+*/
 
-console.log(wb.routing);
+const routing = workbox.routing.default;
+
+const specialImgUrl = new URL('/static/demo/workbox-routing/demo-img.png', location).toString();
+const specialImgRoute = new workbox.routing.Route(({url}) => {
+  return (url === specialImgUrl);
+}, ()=> {
+  return fetch('http://via.placeholder.com/300x300/ffffff/F57C00?text=Hello+from+Workbox');
+});
+routing.registerRoute();
 
 self.addEventListener('message', function(event) {
   switch(event.data.command) {
